86c6d994ca3d1d214570e99efc61efe0f4ce0e85
xhoudek <xhoudek@gmail.com>
2019-05-28 20:56:20 -0500

    added display for level and ships left (wil make it look better later)

--- game_functions.py
+++ game_functions.py
@@ -3,7 +3,7 @@ from time import sleep
 from bullet import Bullet
 from alien import Alien
 
-def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     """respond to keypresses and mouse events"""
     running = True
     for event in pygame.event.get():
@@ -19,18 +19,24 @@ def check_events(ai_settings, screen, stats, play_button, ship, aliens, bullets)
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
-            check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bullets, 
+            check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets, 
                 mouse_x, mouse_y)
 
     return running
 
-def start_game(ai_settings, screen, stats, play_button, ship, aliens, bullets):
+def start_game(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets):
     # hide the mouse cursor
     pygame.mouse.set_visible(False)
 
     # reset the game statistics
     stats.reset_stats()
     stats.game_active = True
+
+    # reset the scoreboard images
+    sb.prep_score()
+    sb.prep_high_score()
+    sb.prep_level()
+    sb.prep_ships()
 
     # empty the list of aliens and bullets
     aliens.empty()
@@ -40,14 +46,14 @@ def start_game(ai_settings, screen, stats, play_button, ship, aliens, bullets):
     create_fleet(ai_settings, screen, ship, aliens)
     ship.center_ship()
 
-def check_play_button(ai_settings, screen, stats, play_button, ship, aliens,
+def check_play_button(ai_settings, screen, stats, sb, play_button, ship, aliens,
     bullets, mouse_x, mouse_y):
     """start a new game when the player clicks Play"""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
         # reset the game settings
         ai_settings.initialize_dynamic_settings()
-        start_game(ai_settings, screen, stats, play_button, ship, aliens, bullets)
+        start_game(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
                 
 def update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button):
     """update images on the screen and flip to the new screen"""
@@ -113,11 +119,17 @@ def check_bullet_alien_collision(ai_settings, screen, stats, sb, ship, aliens, b
         for aliens in collisions.values():
             stats.score += ai_settings.alien_points * len(aliens)
             sb.prep_score()
+        check_high_score(stats, sb)
 
     if len(aliens) == 0:
-        # destroy existing bullets, speed up game, and create new fleet
+        # if the entire fleet is destroyed, start a new level
         bullets.empty()
         ai_settings.increase_speed()
+
+        # increase level
+        stats.level += 1
+        sb.prep_level()
+
         create_fleet(ai_settings, screen,ship, aliens)
 
 def fire_bullet(ai_settings, screen, ship, bullets):
@@ -173,11 +185,14 @@ def change_fleet_direction(ai_settings, aliens):
         alien.rect.y += ai_settings.fleet_drop_speed
     ai_settings.fleet_direction *= -1
 
-def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+def ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """respond to ship being hit by alien"""
     if stats.ships_left > 0:
         # decrement ships_left
         stats.ships_left -= 1
+
+        # update scoreboard
+        sb.prep_ships()
 
         # empty the list of aliens and bullets
         aliens.empty()
@@ -194,16 +209,16 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
         stats.game_active = False
         pygame.mouse.set_visible(True)
 
-def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+def check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """check if any aliens have reached the bottom of the screen"""
     screen_rect = screen.get_rect()
     for alien in aliens.sprites():
         if alien.rect.bottom >= screen_rect.bottom:
             # treat this the same as if the ship got hit
-            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
             break
 
-def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
+def update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets):
     """
     check if the fleet is at an edge,
     and then update the positions of all aliens in the fleet
@@ -213,7 +228,13 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
 
     # look for alien-ship collisions
     if pygame.sprite.spritecollideany(ship, aliens):
-        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+        ship_hit(ai_settings, screen, stats, sb, ship, aliens, bullets)
 
     # look for aliens hitting the bottom of the screen
-    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+    check_aliens_bottom(ai_settings, screen, stats, sb, ship, aliens, bullets)
+
+def check_high_score(stats, sb):
+    """check to see if there is a new high score"""
+    if stats.score > stats.high_score:
+        stats.high_score = stats.score
+        sb.prep_high_score()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 import pygame.font
+from pygame.sprite import Group
+from ship import Ship
 
 class Scoreboard():
     """a class to report scoring information"""
@@ -14,8 +16,11 @@ class Scoreboard():
         self.text_color = (250, 250, 250)
         self.font = pygame.font.SysFont(None, 48)
 
-        # prepare the initial score image
+        # prepare the initial score images
         self.prep_score()
+        self.prep_high_score()
+        self.prep_level()
+        self.prep_ships()
 
     def prep_score(self):
         """turn the score into a rendered image"""
@@ -30,6 +35,44 @@ class Scoreboard():
         self.score_rect.right = self.screen_rect.right - 20
         self.score_rect.top = 20
 
+    def prep_high_score(self):
+        """turn the high score into a rendered image"""
+        high_score = round(self.stats.high_score, -1)
+        high_score_str = 'High Score: ' + '{:,}'.format(high_score)
+        self.high_score_image = self.font.render(
+            high_score_str, True, self.text_color, self.ai_settings.bg_color
+        )
+
+        # center the high score at the top of the screen
+        self.high_score_rect = self.high_score_image.get_rect()
+        self.high_score_rect.centerx = self.screen_rect.centerx
+        self.high_score_rect.top = self.score_rect.top
+
+    def prep_level(self):
+        """turn the level into a rendered image"""
+        self.level_image = self.font.render(
+            'Level ' + str(self.stats.level), True, self.text_color
+        )
+
+        # position the level below the score
+        self.level_rect = self.level_image.get_rect()
+        self.level_rect.right = self.score_rect.right
+        self.level_rect.top = self.score_rect.bottom + 10
+
+    def prep_ships(self):
+        """show how many ships are left"""
+        self.ships = Group()
+        for ship_number in range(self.stats.ships_left):
+            ship = Ship(self.ai_settings, self.screen)
+            ship.rect.x = 10 + ship_number * ship.rect.width
+            ship.rect.y = 10
+            self.ships.add(ship)
+
     def show_score(self):
-        """draw score to the screen"""
+        """draw scores an ships to the screen"""
         self.screen.blit(self.score_image, self.score_rect)
+        self.screen.blit(self.high_score_image, self.high_score_rect)
+        self.screen.blit(self.level_image, self.level_rect)
+
+        # draw ships
+        self.ships.draw(self.screen)
